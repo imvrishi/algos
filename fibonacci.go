9646db4b568200f76439cf84b1733b884817c6e2
Rishikesh Vishwakarma <rishikesh.v@purplle.com>
2021-07-11 02:57:07 +0530

    added memoization

--- fibonacci.go
+++ fibonacci.go
@@ -1,6 +1,9 @@
-package fibonacci
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func fibonacciIterative(n int) int {
 	a := 0
@@ -18,7 +21,24 @@ func fibonacciRecursive(n int) int {
 	return fibonacciRecursive(n-1) + fibonacciRecursive(n-2)
 }
 
+func fibonacciRecursiveMemoized(n int, cache *map[int]int) int {
+	if n < 2 {
+		return n
+	}
+	if v, ok := (*cache)[n]; ok {
+		return v
+	} else {
+		(*cache)[n] = fibonacciRecursiveMemoized(n-1, cache) + fibonacciRecursiveMemoized(n-2, cache)
+		return (*cache)[n]
+	}
+}
+
 func main() {
-	fmt.Println(fibonacciIterative(8))
-	fmt.Println(fibonacciRecursive(8))
+	// start := time.Now()
+	// fmt.Println(fibonacciRecursive(40))
+	// fmt.Println(time.Since(start))
+	cache := &map[int]int{}
+	start := time.Now()
+	fmt.Println(fibonacciRecursiveMemoized(40, cache))
+	fmt.Println(time.Since(start))
 }
